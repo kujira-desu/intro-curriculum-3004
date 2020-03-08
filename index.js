b7405b1bc7398b44d8ff3880e7873c61188be7fd
kujira_desu <test@test.hoge.hoge>
2020-03-08 11:33:46 +0900

    multi関数を追加

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 'use strict';
+
+/**
+ * 引数で受け取った数値配列を全て足す
+ */
 function add(numbers) {
   let result = 0;
   for (let num of numbers) {
@@ -6,4 +10,16 @@ function add(numbers) {
   }
   return result;
 }
-module.exports = { add };
+
+/**
+ * 引数で受け取った数値配列を全て掛ける
+ */
+function multi(numbers) {
+  let result = 1;
+  for (let num of numbers) {
+    result = result * num;
+  }
+  return result;
+}
+
+module.exports = { add, multi };
